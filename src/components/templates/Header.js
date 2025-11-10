eaa8a3e44b9356579e9cda8a5b357efd1c7a2eb1
shayan <shayanjafarnejad75@gmail.com>
2025-11-10 13:40:39 +0330

    develop header section

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -11,7 +11,13 @@ function Header() {
 
   return (
     <div className='container'>
-
+      <header className='header'>
+        <div className='container-texts-header'>
+          <h1 className='title-header'>The Perfect Moment Between Past And Future</h1>
+          <button className='btn-header'>Buy Now</button>
+        </div>
+        <div className='container-model'></div>
+      </header>
     </div>
   )
 }
