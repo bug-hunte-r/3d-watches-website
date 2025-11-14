932a5f5eeb136a7c82beba866854bbe76cd04331
shayan <shayanjafarnejad75@gmail.com>
2025-11-14 08:06:37 +0330

    change the background and the cards glasses

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import DarkVeil from "@/anim/DarkVeil";
+import LiquidChrome from "@/anim/LiquidChrome ";
 import Header from "../components/templates/Header"
 import BestSeller from "@/components/templates/BestSeller";
 
@@ -7,13 +7,16 @@ export default function Home() {
 
   return (
     <>
-
       <div className="hyperSpeed">
-        <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-          <DarkVeil />
+        <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+          <LiquidChrome
+            baseColor={[0, 0.6, 0.7]}
+            speed={0.8}
+            amplitude={0.1}
+            interactive={false}
+          />
         </div>
       </div>
-
       <Header />
       <BestSeller />
     </>
--- src/components/modules/BestSellerCard.js
+++ src/components/modules/BestSellerCard.js
@@ -8,53 +8,51 @@ import gsap from 'gsap'
 
 function BestSellerCard() {
 
-    const products = [
-        { id: 1, img: '/imgs/img.avif', title: 'Casio Watch', desc: 'The best and strongest watch', price: 100 },
-        { id: 2, img: '/imgs/watch2.png', title: 'Huawei Watch', desc: 'The smartest and cleaner watch', price: 136 },
-        { id: 3, img: '/imgs/watch3.webp', title: 'Apple Watch', desc: 'The strongest and biggest watch', price: 198 }
-    ]
+  const products = [
+    { id: 1, img: '/imgs/img.avif', title: 'Casio Watch', desc: 'The best and strongest watch', price: 100 },
+    { id: 2, img: '/imgs/watch2.png', title: 'Huawei Watch', desc: 'The smartest and cleaner watch', price: 136 },
+    { id: 3, img: '/imgs/watch3.webp', title: 'Apple Watch', desc: 'The strongest and biggest watch', price: 198 }
+  ]
 
-    useEffect(() => {
-        gsap.registerPlugin(_ScrollTrigger);
-      
-        _ScrollTrigger.batch(".best-seller-cards", {
-          onEnter: (batch) => {
-            gsap.to(batch, {
-              opacity: 1,
-              y: 0,
-              duration: 0.7,
-              stagger: 0.5,
-            });
-          },
-          onLeaveBack: (batch) => {
-            // وقتی کاربر اسکرول بالا برمی‌گرده کارت‌ها رو دوباره مخفی کن
-            gsap.to(batch, {
-              opacity: 0,
-              y: 50,
-              duration: 1,
-              stagger: 0.2,
-              ease: "power2.out",
-            });
-          },
-          start: "top 90%",
-          end: "bottom top",
+  useEffect(() => {
+    gsap.registerPlugin(_ScrollTrigger);
+
+    _ScrollTrigger.batch(".best-seller-cards", {
+      onEnter: (batch) => {
+        gsap.to(batch, {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          stagger: 0.5,
         });
-      }, []);
-      
+      },
+      onLeaveBack: (batch) => {
+        gsap.to(batch, {
+          opacity: 0,
+          y: 50,
+          duration: 0.5,
+          stagger: 0.5,
+        });
+      },
+      start: "top 70%",
+      end: "bottom 30%",
+    });
+  }, []);
 
-    return (
-        <>
-            {products.map((product) => (
-                <div className='best-seller-cards' key={product.id}>
-                    <Image src={product.img} width={500} height={500} alt='watch' className='img-best-seller-card' />
-                    <h3 className='title-best-seller-card'>{product.title}</h3>
-                    <p className='desc-best-seller-card'>{product.desc}</p>
-                    <p className='price-best-seller-card'>{product.price}$</p>
-                    <button className='btn-best-seller-card'>Buy</button>
-                </div>
-            ))}
-        </>
-    )
+
+  return (
+    <>
+      {products.map((product) => (
+        <div className='best-seller-cards' key={product.id}>
+          <Image src={product.img} width={500} height={500} alt='watch' className='img-best-seller-card' />
+          <h3 className='title-best-seller-card'>{product.title}</h3>
+          <p className='desc-best-seller-card'>{product.desc}</p>
+          <p className='price-best-seller-card'>{product.price}$</p>
+          <button className='btn-best-seller-card'>Buy</button>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default BestSellerCard
