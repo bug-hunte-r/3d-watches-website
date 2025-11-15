d64e8a820b0dece0fba47e897951da1dd35974af
shayan <shayanjafarnejad75@gmail.com>
2025-11-15 13:38:54 +0330

    set new background

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,28 @@ import Header from "../components/templates/Header"
 import BestSeller from "@/components/templates/BestSeller";
 import './globals.css'
 import Counter from "@/components/templates/Counter";
+import FloatingLines from "@/anim/FloatingLines";
 
 export default function Home() {
 
   return (
     <>
+      <div className="background">
+        <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+          <FloatingLines
+            enabledWaves={['top', 'middle', 'bottom']}
+            // Array - specify line count per wave; Number - same count for all waves
+            lineCount={[20, 20, 20]}
+            // Array - specify line distance per wave; Number - same distance for all waves
+            lineDistance={[100, 100, 100]}
+            bendRadius={30}
+            bendStrength={-0.5}
+            interactive={false}
+            parallax={false}
+          />
+        </div>
+
+      </div>
       <Header />
       <BestSeller />
       <Counter />
--- src/components/templates/Counter.js
+++ src/components/templates/Counter.js
@@ -8,15 +8,15 @@ function Counter() {
         <div className='container'>
             <div className='container-counter'>
                 <div className='container-text-and-number-counter'>
-                    <CountUp from={0} to={252} separator="," direction="up" duration={1} className="number-counter" />
+                    <CountUp from={0} to={252} separator="," direction="up" duration={2} className="number-counter" />
                     <h1 className='title-counter'>happy customers</h1>
                 </div>
                 <div className='container-text-and-number-counter'>
-                    <CountUp from={0} to={10} separator="," direction="up" duration={1} className="number-counter" />
+                    <CountUp from={0} to={10} separator="," direction="up" duration={2} className="number-counter" />
                     <h1 className='title-counter'>years experiance</h1>
                 </div>
                 <div className='container-text-and-number-counter'>
-                    <CountUp from={0} to={67} separator="," direction="up" duration={1} className="number-counter" />
+                    <CountUp from={0} to={67} separator="," direction="up" duration={2} className="number-counter" />
                     <h1 className='title-counter'>products</h1>
                 </div>
             </div>
