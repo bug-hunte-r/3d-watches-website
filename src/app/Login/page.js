5a200064e3cd70d09c3e24b2dfe002a913754583
shayan <shayanjafarnejad75@gmail.com>
2025-11-16 14:13:06 +0330

    set background for login form

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -3,21 +3,33 @@ import style from '../../style/AuthForms/AuthForms.css'
 import media from '../../style/AuthForms/mediaAuthForms.css'
 import { IoIosArrowBack } from "react-icons/io";
 import Link from 'next/link';
+import Silk from '@/anim/Silk';
 
 function Login() {
     return (
-        <div className='container'>
-            <div className='container-form'>
-                <div className='container-title-and-back-icon-form'>
-                    <Link href={'/'}><IoIosArrowBack className='icon-back-form' /></Link>
-                    <h1 className='title-form'>Login</h1>
-                </div>
-                <input type='text' placeholder='Username or Email' className='inputs-form' />
-                <input type='password' placeholder='Password' className='inputs-form' />
-                <button className='btn-form'>Login</button>
-                <p className='text-under-form'>Dont have an account? <Link className='link-under-form' href={'/Signup'}>Signup</Link></p>
+        <>
+            <div className='container-silk'>
+                <Silk
+                    speed={5}
+                    scale={1}
+                    color="#3300ff"
+                    noiseIntensity={0}
+                    rotation={0}
+                />
             </div>
-        </div>
+            <div className='container'>
+                <div className='container-form'>
+                    <div className='container-title-and-back-icon-form'>
+                        <Link href={'/'}><IoIosArrowBack className='icon-back-form' /></Link>
+                        <h1 className='title-form'>Login</h1>
+                    </div>
+                    <input type='text' placeholder='Username or Email' className='inputs-form' />
+                    <input type='password' placeholder='Password' className='inputs-form' />
+                    <button className='btn-form'>Login</button>
+                    <p className='text-under-form'>Dont have an account? <Link className='link-under-form' href={'/Signup'}>Signup</Link></p>
+                </div>
+            </div>
+        </>
     )
 }
 
