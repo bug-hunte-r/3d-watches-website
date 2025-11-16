4e417f7b46afe21f65aad830c65642bae5782183
shayan <shayanjafarnejad75@gmail.com>
2025-11-16 12:28:00 +0330

    develop sign up form

--- src/app/Signup/page.js
+++ src/app/Signup/page.js
@@ -3,9 +3,17 @@ import style from '../../style/AuthForms/AuthForms.css'
 import media from '../../style/AuthForms/mediaAuthForms.css'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div className='container'>
+            <div className='container-form'>
+                <h1 className='title-form'>Sign up</h1>
+                <input type='text' placeholder='Username' className='inputs-form' />
+                <input type='email' placeholder='Email' className='inputs-form' />
+                <input type='pass' placeholder='Password' className='inputs-form' />
+                <button className='btn-form'>Sign up</button>
+            </div>
+        </div>
+    )
 }
 
 export default Signup
