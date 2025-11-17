209f817f95f3c94f727561597492f14dd904cdd2
shayan <shayanjafarnejad75@gmail.com>
2025-11-17 11:21:40 +0330

    develop classical section

--- src/app/Shop/page.js
+++ src/app/Shop/page.js
@@ -50,12 +50,12 @@ function Shop() {
         <div className='container-whatches-category'>
           <div className='container-classic-watches'>
             <h2 className='title-classic-watches'>Classical Watches</h2>
+            <Image src={'/imgs/clas.webp'} width={500} height={500} alt='classic' className='img-classic-watch' />
+          </div>
+          {/* <div className='container-classic-watches'>
+            <h2 className='title-classic-watches'>Modern Watches</h2>
             <Image src={'/imgs/classic.webp'} width={500} height={500} alt='classic' className='img-classic-watch' />
-          </div>
-          <div className='container-modern-watches'>
-            <h2 className='title-modern-watches'>Modern Watches</h2>
-            <Image src={'/imgs/modern.webp'} width={500} height={500} alt='modern' className='img-modern-watch' />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
