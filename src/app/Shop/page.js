4b2d647a381e4016a32ca526672d26b0c3ac5e40
shayan <shayanjafarnejad75@gmail.com>
2025-11-17 08:28:23 +0330

    create main files for shops route

---
+++ src/app/Shop/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from '../../style/Shop/Shop.css'
+import media from '../../style/Shop/mediaShop.css'
+
+function Shop() {
+  return (
+    <div>Shop</div>
+  )
+}
+
+export default Shop
