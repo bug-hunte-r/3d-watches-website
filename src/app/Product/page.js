35cd0a38bf370548b9af89c0fc1f99d0f3a8dcbb
shayan <shayanjafarnejad75@gmail.com>
2025-11-18 13:14:55 +0330

    set navbar for product page

--- src/app/Product/page.js
+++ src/app/Product/page.js
@@ -1,10 +1,17 @@
+"use client"
 import React from 'react'
 import style from '../../style/Product/Product.css'
 import media from '../../style/Product/mediaProduct.css'
+import Nav from '@/components/templates/Nav'
 
 function Product() {
   return (
-    <div>Product</div>
+    <div className='container'>
+      <Nav />
+      <div className='container-product-detail'>
+        
+      </div>
+    </div>
   )
 }
 
