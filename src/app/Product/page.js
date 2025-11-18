d3451e5cf3c6292997c06aa4f919ade326f2e4d4
shayan <shayanjafarnejad75@gmail.com>
2025-11-18 12:36:13 +0330

    create products details files

---
+++ src/app/Product/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from '../../style/Product/Product.css'
+import media from '../../style/Product/mediaProduct.css'
+
+function Product() {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
