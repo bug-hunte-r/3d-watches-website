431ce313e718367ff88008acef2b821b01534726
shayan <shayanjafarnejad75@gmail.com>
2025-11-19 10:00:12 +0330

    set navbar for cart page

--- src/app/Cart/page.js
+++ src/app/Cart/page.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import style from '../../style/Cart/Cart.css'
 import media from '../../style/Cart/mediaCart.css'
+import Nav from '@/components/templates/Nav'
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <div className='container'>
+        <Nav />
+    </div>
   )
 }
 
