01a5133956b5bb042c6179512d1ea75949399f43
shayan <shayanjafarnejad75@gmail.com>
2025-11-19 10:07:31 +0330

    develop carrt cards template

--- src/app/Cart/page.js
+++ src/app/Cart/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 import style from '../../style/Cart/Cart.css'
 import media from '../../style/Cart/mediaCart.css'
 import Nav from '@/components/templates/Nav'
+import CartCards from '@/components/modules/CartCards'
 
 function Cart() {
     return (
@@ -10,6 +11,9 @@ function Cart() {
             <Nav />
             <div className='container-cart'>
                 <h1 className='title-cart'>Cart</h1>
+                <div className='container-cart-cards'>
+                    <CartCards />
+                </div>
             </div>
         </div>
     )
--- src/components/modules/CartCards.js
+++ src/components/modules/CartCards.js
@@ -1,8 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 
 function CartCards() {
+
+  const products = [
+    { id: 1, img: '/imgs/img.avif', title: 'Casio Watch', desc: 'The best and strongest watch', price: 100 },
+    { id: 2, img: '/imgs/watch2.png', title: 'Huawei Watch', desc: 'The smartest and cleaner watch', price: 136 },
+    { id: 3, img: '/imgs/watch3.webp', title: 'Apple Watch', desc: 'The strongest and biggest watch', price: 198 }
+  ]
+
   return (
-    <div>CartCards</div>
+    <>
+      {products.map((product) => (
+        <div className='cart-card' key={product.id}>
+          <Image width={500} height={500} alt='watch-img' className='img-cart-card' src={product.img} />
+          <div className='container-cart-cards-texts'>
+            <h2 className='title-product-cart-card'>{product.title}</h2>
+            <p className='desc-product-cart-card'>{product.desc}</p>
+            <p className='price-product-cart-card'>{product.price}</p>
+            <div className='container-add-and-remove-product-count'>+ 1 -</div>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
