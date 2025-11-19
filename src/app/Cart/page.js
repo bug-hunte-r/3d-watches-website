af361b22d3036d315047e2bd50ff19cfcccc7395
shayan <shayanjafarnejad75@gmail.com>
2025-11-19 10:01:13 +0330

    develop cart page templates

--- src/app/Cart/page.js
+++ src/app/Cart/page.js
@@ -1,14 +1,18 @@
+"use client"
 import React from 'react'
 import style from '../../style/Cart/Cart.css'
 import media from '../../style/Cart/mediaCart.css'
 import Nav from '@/components/templates/Nav'
 
 function Cart() {
-  return (
-    <div className='container'>
-        <Nav />
-    </div>
-  )
+    return (
+        <div className='container'>
+            <Nav />
+            <div className='container-cart'>
+                <h1 className='title-cart'>Cart</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
