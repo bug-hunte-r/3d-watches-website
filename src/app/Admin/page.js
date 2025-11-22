03780aac6cbe0006ef5de2c8557bfe2e9e1ac9e2
shayan <shayanjafarnejad75@gmail.com>
2025-11-22 15:37:03 +0330

    make array for users data and side bar items

--- src/app/Admin/page.js
+++ src/app/Admin/page.js
@@ -15,6 +15,30 @@ import { CgMenuGridO } from "react-icons/cg";
 function Admin() {
 
     const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+    const [isSideBarItemActive, setIsSideBarItemActive] = useState(1)
+
+    const usersInfo = [
+        { id: 1, name: 'Shayan', email: 'shayan@gmail.com', role: 'ADMIN' },
+        { id: 2, name: 'Kian', email: 'Kian@gmail.com', role: 'USER' },
+        { id: 3, name: 'MMd', email: 'MMd@gmail.com', role: 'USER' },
+        { id: 4, name: 'Ahmad', email: 'ahmad@gmail.com', role: 'USER' },
+        { id: 5, name: 'babak', email: 'babak@gmail.com', role: 'USER' },
+        { id: 6, name: 'sara', email: 'sara@gmail.com', role: 'USER' },
+        { id: 7, name: 'Mostafa', email: 'Mostafa@gmail.com', role: 'USER' },
+        { id: 8, name: 'parham', email: 'parham@gmail.com', role: 'USER' },
+        { id: 9, name: 'Arina', email: 'Arina@gmail.com', role: 'USER' },
+        { id: 10, name: 'Mahmod', email: 'Mahmod@gmail.com', role: 'USER' },
+    ]
+
+    const sideBarItems = [
+        { id: 1, text: 'Users', icon: FiUsers },
+        { id: 2, text: 'Products', icon: IoWatchOutline },
+        { id: 3, text: 'Profile', icon: LiaUserEditSolid },
+        { id: 4, text: 'Orders', icon: MdOutlineShoppingBag },
+        { id: 5, text: 'Tickets', icon: LiaTicketAltSolid },
+        { id: 6, text: 'Comments', icon: BiCommentDetail },
+        { id: 7, text: 'Sign out', icon: GoSignOut }
+    ]
 
     return (
         <div className='container'>
@@ -24,34 +48,15 @@ function Admin() {
                 <div className={`container-left-bar-admin ${isSideBarOpen ? 'opened-side-bar' : 'closed-side-bar'}`}>
                     <p className='close-btn-side-bar' onClick={() => setIsSideBarOpen(false)}>X</p>
                     <h2 className='username-in-side-bar'>Welcome Shayan!</h2>
-                    <div className='items-left-bar-admin items-left-bar-admin-active'>
-                        <FiUsers className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Users</p>
-                    </div>
-                    <div className='items-left-bar-admin'>
-                        <IoWatchOutline className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Products</p>
-                    </div>
-                    <div className='items-left-bar-admin'>
-                        <LiaUserEditSolid className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Profile</p>
-                    </div>
-                    <div className='items-left-bar-admin'>
-                        <MdOutlineShoppingBag className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Orders</p>
-                    </div>
-                    <div className='items-left-bar-admin'>
-                        <LiaTicketAltSolid className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Tickets</p>
-                    </div>
-                    <div className='items-left-bar-admin'>
-                        <BiCommentDetail className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Comments</p>
-                    </div>
-                    <div className='items-left-bar-admin bottom'>
-                        <GoSignOut className='icons-left-bar-items' />
-                        <p className='title-left-bar-items'>Sign out</p>
-                    </div>
+                    {sideBarItems.map((item) => {
+                        const Icon = item.icon
+                        return (
+                            <div className={`items-left-bar-admin ${isSideBarItemActive === item.id ? 'items-left-bar-admin-active' : ''}`} key={item.id} onClick={() => {setIsSideBarItemActive(item.id); setIsSideBarOpen(false)}}>
+                                <Icon className='icons-left-bar-items' />
+                                <p className='title-left-bar-items'>{item.text}</p>
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className="user-list-container">
                     <h1 className='title-user-list'>Users List</h1>
@@ -60,81 +65,13 @@ function Admin() {
                         <p className='titles-user-infos'>Email</p>
                         <p className='titles-user-infos'>Role</p>
                     </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
-                    <div className='container-users-info'>
-                        <p className='users-info'>Shayan</p>
-                        <p className='users-info'>shayan@gmail.com</p>
-                        <p className='users-info'>Admin</p>
-                    </div>
+                    {usersInfo.map((info) => (
+                        <div className='container-users-info' key={info.id}>
+                            <p className='users-info'>{info.name}</p>
+                            <p className='users-info'>{info.email}</p>
+                            <p className='users-info'>{info.role}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
