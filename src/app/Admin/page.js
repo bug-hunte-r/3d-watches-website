2c12492462b614aeda3a337fd32157114cf59e07
shayan <shayanjafarnejad75@gmail.com>
2025-11-22 08:40:42 +0330

    create admin files and folders

---
+++ src/app/Admin/page.js
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+import style from '../../style/Admin/Admin.css'
+import media from '../../style/Admin/mediaAdmin.css'
+import Nav from '@/components/templates/Nav'
+
+function Admin() {
+  return (
+    <div className='container'>
+        <Nav />
+        <div className='container-admin'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Admin
